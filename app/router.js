80bbd8aa5cb45a2edddaea882d7a89b122055af1
郭海潮 <guohaichao@longshine.com>
2020-09-27 17:51:59 +0800

    up

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ module.exports = app => {
   const { router, controller ,config} = app;
   const jwt = app.middleware.jwt( config.jwt );
   router.post('/api/login', controller.adminList.login);
-  router.get('/api/loginout',jwt, controller.adminList.loginout);
+  router.get('/api/loginout',controller.adminList.loginout);
   router.get('/api/getInfo',jwt, controller.adminList.getInfo);
+	router.get('/api/menu',jwt, controller.menu.getMenu);
 };
